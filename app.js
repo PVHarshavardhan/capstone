658d3c4d0e5908cf93773bdf58b837da734febe6
PVHarshavardhan <pvharsha@LAPTOP-TVAAEPR0.localdomain>
2024-02-02 13:53:54 +0530

    pages

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require("path");
-const { Coursesall, Page } = require('./models')
+const { Coursesall, Pages } = require('./models')
 const bodyParser = require('body-parser')
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -37,9 +37,6 @@ app.post("/chapters",async (request, response) => {
     const des = request.body.description;
     console.log("hell0"+courseName)
     console.log(des);
-        
-    
-
     try{
         const course = await Coursesall.create({
             coursename:courseName,
@@ -53,9 +50,17 @@ app.post("/chapters",async (request, response) => {
         console.log(error)
     }
 
-    response.render("page",{coursename:courseName,chaptername:chapterName})
+    const pagesList = Pages.getPages(courseName,chapterName); 
+    console.log(pagesList)
+    response.render("page",{coursename:courseName,chaptername:chapterName,pageslist: pagesList})
 })
 
+app.post("/page", (request, response) => {
+    const courseName=request.body.coursename;
+    const chapterName=request.body.chaptername;
+
+    response.render("page",{coursename:courseName,chaptername:chapterName})
+})
 app.post("/pagecreation",async (request,response) => {
     const courseName = request.body.coursename;
     const chapters = await Coursesall.getChapters(courseName);
@@ -63,8 +68,22 @@ app.post("/pagecreation",async (request,response) => {
     
 })
 
-app.post("/showpage", (request, response) => {
-    console.log(request.body.showtitle)
+app.post("/showpage", async (request, response) => {
+    try {
+        console.log(request.body.coursename,request.body.chaptername,request.body.pagename);
+        const page = await Pages.create({
+            pagename:request.body.pagename,
+            content:request.body.content,
+            coursename:coursename,
+            chaptername:request.body.chaptername
+        })
+    } catch(error){
+        console.log(error)
+    }
+    response.render("displaypage",{ pagename:request.body.pagename,
+        content:request.body.content,
+        coursename:request.body.coursename,
+        chaptername:request.body.chaptername })
 
 })
 module.exports = app;
---
+++ models/pages.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pages extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+    static getPages(coursename, chaptername) {
+      return this.findAll({where:{coursename,chaptername}})
+    }
+  }
+  Pages.init({
+    pagename: DataTypes.STRING,
+    content: DataTypes.STRING,
+    coursename: DataTypes.STRING,
+    chaptername: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Pages',
+  });
+  return Pages;
+};
